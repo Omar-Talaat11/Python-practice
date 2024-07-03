5c9f0e0155ace50cc0bb1df9d2ea7a83433af736
Omar Tal3at <omar_tal3at@Omar.Tal3at>
2024-07-03 15:28:21 +0300

    alexa auto commit

--- Alexa/alexa.py
+++ Alexa/alexa.py
@@ -5,32 +5,117 @@ import playsound
 from gtts import gTTS
 import random
 import speech_recognition as sr
-import threading
+import pyautogui
 
 
-import speech_recognition as sr
 
-# Initialize recognizer
-recognizer = sr.Recognizer()
 
-# Use microphone as source
+class Alexa():
+    
+    
+    def __init__(self):
+        pass
+    
+    def text_to_sound(self,text,filename):
+        sound=gTTS(text=text,lang='ar',slow='False')
+        sound.save(filename)
+        
+    def Alexa_Speak(self,filename):
+        playsound.playsound(filename)
 
-playsound.playsound("my name is Omar")
-with sr.Microphone() as source:
-    print("Listening...")
 
-    # Adjust for ambient noise
-    recognizer.adjust_for_ambient_noise(source)
+    def Alexa_recognize_speech(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Speak something...")
+            audio = r.listen(source,phrase_time_limit=5)
+        
+        try:
+            print("Recognizing...")
+            text = r.recognize_google(audio,language='ar')
+            print(f"You said: {text}")
+            return text
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print(f"Error fetching results; {e}")
 
-    # Listen for audio and capture it in 'audio' variable
-    audio = recognizer.listen(source)
+    def find_words(self,words,speech):
+        for word in words:
+            if(word in speech):
+                return True
+        return False
 
-    try:
-        # Recognize speech using Google Speech Recognition
-        text = recognizer.recognize_google(audio)
-        print(f"You said: {text}")
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand the audio.")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
+    def Alexa_GM(self):
+        self.text_to_sound("صباح الخير يا عمر","my_file.mp3")
+        self.Alexa_Speak("my_file.mp3")
+        os.system("rm my_file.mp3")
+        
+    def Alexa_TD(self):
+        current_time_struct = time.localtime()
+        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time_struct)
+        day=formatted_time.split()[0]
+        t=formatted_time.split()[1]
+        self.text_to_sound("اليوم هو "+day+" و الساعة الان هي "+t,"my_file.mp3")
+        self.Alexa_Speak("my_file.mp3")
+        os.system("rm my_file.mp3")
+        
+    def Alexa_Open_CGPT(self):
+        webbrowser.get('firefox').open("https://chatgpt.com/")
+        
+    def Alexa_Open_VSCODE(self):
+        os.system("code")
+    
+    def Alexa_Terminal(self):
+        pyautogui.hotkey('ctrl','alt','t')
+        
+    def Alexa_GIT_Commit(self):
+        os.system("cd /home/omar_tal3at/Emb_Linux/tasks/Python")
+        os.system("git add -A")
+        os.system('git commit -m "alexa auto commit"')
+        os.system("git branch -M main")
+        os.system("git remote add origin repo https://github.com/Omar-Talaat11/Python-practice.git")
+        os.system("git push -u origin main")
+        
+        
+        
+        
+        
+        
+        
+    def Alexa_Respond(self,speech):
+        if(self.find_words(["صباح","خير"],speech)):
+            self.Alexa_GM()
+            
+        if(self.find_words(["يوم","ساعه","تاريخ"],speech)):
+            self.Alexa_TD()
+            
+        if(self.find_words(["شات","ج ب ت"],speech)):
+            self.Alexa_Open_CGPT()
+            
+        if(self.find_words(["الكود"],speech)):
+            self.Alexa_Open_VSCODE()
+        
+        if(self.find_words(["تيرمينال"],speech)):
+            self.Alexa_Terminal()
+            
+        if(self.find_words(["كوميت","جيت","جيتهب"],speech)):
+            self.Alexa_GIT_Commit()
+            
+    
+    
 
+        
+    
+
+
+
+alexa=Alexa()
+
+
+
+while 1:
+    recognizer = sr.Recognizer()
+    alexa=Alexa()
+    recognized_text = alexa.Alexa_recognize_speech()
+    alexa.Alexa_Respond(recognized_text)
